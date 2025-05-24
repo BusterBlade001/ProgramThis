6d96d0f82889c6e803580c19a5a9109f107cdb26
Cesar <cesarnikolas41@gmail.com>
2025-05-24 17:34:52 -0400

    Se suben archvios de la plantilla

---
+++ src/test/java/com/programthis/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programthis.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
